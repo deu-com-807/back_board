ac9ae4cd70f3bc70e743245e1c5debf6c9a679ef
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2023-02-13 04:09:23 +0900

    update

--- src/main/java/lab/back_board/BackBoardApplication.java
+++ src/main/java/lab/back_board/BackBoardApplication.java
@@ -2,12 +2,17 @@ package lab.back_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackBoardApplication {
+public class BackBoardApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackBoardApplication.class);
+	}
+	public static void main(final String[] args) {
 		SpringApplication.run(BackBoardApplication.class, args);
 	}
-
 }
